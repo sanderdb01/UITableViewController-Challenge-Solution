262a0a425c4e92cc163e0251f9a7b39d2572a2f7
David Sanders <sanderdb01@gmail.com>
2014-03-24 13:55:17 -0400

    Initial Commit

---
+++ UITableViewController Challenge Solution/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by David Sanders on 3/24/14.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableViewController Challenge Solution/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by David Sanders on 3/24/14.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
